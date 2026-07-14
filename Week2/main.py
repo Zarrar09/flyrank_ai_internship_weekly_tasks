a45ff249650cf718eb1d4c893a64c1217f133f2c
Zarrar09 <zarrarnadeemmalik@gmail.com>
2026-07-14 18:53:09 +0100

    Week2 | Stage3 | created validation + insertion of new tasks

--- Week2/main.py
+++ Week2/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from fastapi import Request
 from fastapi.responses import JSONResponse
 from datetime import datetime
 
 app = FastAPI()
+
+#To add tasks: curl.exe -i -X POST http://localhost:8000/tasks -H "Content-Type: application/json" -d "{\"title\": \"Buy milk\"}"
 
 tasks = [
     {
@@ -41,3 +44,25 @@ def getTask(id : int):
             return JSONResponse(status_code=200, content=tasks[i])
     else:
         return JSONResponse(status_code=404, content={"error": f"Task {id} not found"})
+    
+@app.post("/tasks")
+async def addTask(request: Request):
+    body = await request.json()
+    title = body.get("title")
+    
+    if not title:
+        return JSONResponse(status_code=400, content={"error": "You did not provide a title"})
+    
+    maxID = 0
+    for i in range(len(tasks)):
+        if int(tasks[i]["id"]) > maxID:
+            maxID = int(tasks[i]["id"])
+    maxID = int(tasks[lastIndex]["id"]) + 1
+    tasks.append(
+        {
+            "id": maxID,
+            "title": title,
+            "done": False,
+        }
+    )
+    return JSONResponse(status_code=201, content="Created, the polite way to say done.")

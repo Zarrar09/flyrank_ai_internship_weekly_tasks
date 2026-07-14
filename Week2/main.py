e39b4f19566f25917e70269a741c48d9a72a6f79
Zarrar09 <zarrarnadeemmalik@gmail.com>
2026-07-14 18:24:28 +0100

    Week2 | Stage2 | read endpoints with 404 and 200

--- Week2/main.py
+++ Week2/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from datetime import datetime
 
 app = FastAPI()
+
+tasks = [
+    {
+        "id": 99,
+        "title": "FirstAPI",
+        "done": False ,
+    },
+    {
+        "id": 100,
+        "title": "Complete GitHub Repo",
+        "done": False, 
+    },
+    {
+        "id": 101,
+        "title": "Complete Assignment 2",
+        "done": False, 
+    },
+]
 
 @app.get('/')
 async def root():
@@ -10,3 +29,15 @@ async def root():
 @app.get('/health', status_code = 200)
 def getHealth():
     return {"status": "ok"}
+
+@app.get('/tasks')
+def allTasks():
+    return tasks
+    
+@app.get("/tasks/{id}")
+def getTask(id : int):
+    for i in range(len(tasks)):
+        if id == tasks[i]['id']:
+            return JSONResponse(status_code=200, content=tasks[i])
+    else:
+        return JSONResponse(status_code=404, content={"error": f"Task {id} not found"})

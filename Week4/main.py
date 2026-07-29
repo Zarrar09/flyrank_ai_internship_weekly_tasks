046c1381342ddd353bf5f0e2a3b81e4f33458d27
Zarrar09 <zarrarnadeemmalik@gmail.com>
2026-07-29 16:05:03 +0300

    Week 4 | Stage 1 | Got signup and login to work, and returns the correct access and refresh tokens returned by supabase

--- Week4/main.py
+++ Week4/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from supabase_client import supabase
+from pydantic import BaseModel 
 
 app = FastAPI()
+
+class SignUpRequest(BaseModel):
+    email: str
+    password: str
+    
+class LoginRequest(BaseModel):
+    email: str
+    password: str
 
 @app.on_event("startup")
 async def startup():
@@ -10,3 +20,49 @@ async def startup():
 @app.get("/")
 async def root():
     return {"status": "ok"}
+
+@app.post("/auth/signup")
+async def signup(signup: SignUpRequest):
+    email = signup.email
+    password = signup.password
+    
+    if not email or not password:
+        return JSONResponse(status_code=400, content={"error" : "Missing email or password"})
+    
+    
+    credentials = {
+        "email" : email,
+        "password" : password
+    }
+    
+    try:
+        response = supabase.auth.sign_up(credentials)
+        return JSONResponse(status_code=201, content={"user": response.user.model_dump(mode="json")})
+    except Exception as e:
+        return JSONResponse(status_code=400, content={"error": str(e)})
+    
+    
+@app.post("/auth/login")
+async def login(loginDetails : LoginRequest):
+    email = loginDetails.email
+    password = loginDetails.password
+    
+    if not password:
+        return JSONResponse(status_code=400, content={"error": "Missing Password"})
+    
+    credentials = {
+        "email": email,
+        "password" : password
+    }
+    
+    try:
+        response = supabase.auth.sign_in_with_password(credentials)
+        return JSONResponse(
+            status_code=200, 
+            content={
+            "access_token": response.session.access_token,
+            "refresh_token": response.session.refresh_token
+            }
+        )
+    except:
+        return JSONResponse(status_code=401, content={"error": "Invalid Credentials"})
